7b45c51ca8767e7af9c542c222ddcf7fb82b97b4
nasbf <mo0ma@hotmail.com>
2025-10-13 17:48:04 -0500

    sort by category

--- js/main.js
+++ js/main.js
@@ -26,13 +26,55 @@ async function loadPets() {
         });
 
         const data = await res.json();
-        displayPets(data); // aquí pasamos la data al utils
+        displayPets(data); // viene de data utils.js
     } catch (error) {
         console.error("Error cargando mascotas:", error);
     }
 }
 
-document.addEventListener("DOMContentLoaded", loadPets);
+// document.addEventListener("DOMContentLoaded", loadPets);
+
+async function sortPets(filter = "") {
+  try {
+    const token = await getAccessToken();
+
+    
+    const url = `https://api.petfinder.com/v2/animals?${filter}`;
+     
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await res.json();
+    displayPets(data); 
+  } catch (error) {
+    console.error("Error cargando mascotas:", error);
+  }
+}
+document.addEventListener("DOMContentLoaded", () => {
+  loadPets();
+  const filterSelect = document.getElementById("sort");
+
+  filterSelect.addEventListener("change", (event) => {
+    const value = event.target.value;
+
+    // Construir el filtro para cada categoria
+    let filter = "";
+    if (value === "Dogs") filter = "type=dog";
+    else if (value === "Cats") filter = "type=cat";
+    else if (value === "Male") filter = "gender=male";
+    else if (value === "Female") filter = "gender=female";
+    else if (value === "Baby") filter = "age=baby";
+    else if (value === "Adult") filter = "age=adult";
+
+    
+    sortPets(filter);
+  });
+});
+
+
 document.addEventListener("DOMContentLoaded", () => {
     loadFooter();
 });
